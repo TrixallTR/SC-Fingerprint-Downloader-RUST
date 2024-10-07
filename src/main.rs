0849a4e4d21ef5387568151c4943ad77b256d963
TrixallTR <54065782+TrixallTR@users.noreply.github.com>
2024-10-07 20:44:25 +0300

    Add SHA support

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,8 @@ use tokio::{self, sync::Semaphore, task};
 
 #[tokio::main]
 async fn main() {
-    let mut fp = String::new();
-    let mut url = String::new();
-
-    println!("Fingerprint File (e.g. fingerprint.json): ");
-    io::stdin().read_line(&mut fp).unwrap();
-
-    println!("Asset URL: (e.g. https://game-assets.brawlstarsgame.com): ");
-    io::stdin().read_line(&mut url).unwrap();
-
-    url = url.trim().to_string();
-
-    if !url.ends_with('/') {
-        url.push('/');
-    }
-
-    download(fp, url, 10).await;
+    let (fp, url, is_file) = get_config();
+    download(fp, url, is_file, 10).await;
 }
 
 async fn download_file(client: &reqwest::Client, url: String, file_path: String, file_name: String) {
@@ -56,8 +42,14 @@ async fn download_file(client: &reqwest::Client, url: String, file_path: String,
     }
 }
 
-async fn download(fp: String, url: String, threads: usize) {
-    let contents = fs::read_to_string(fp.trim()).unwrap();
+async fn download(fp: String, url: String, is_file: bool, threads: usize) {
+
+    let contents = if is_file {
+        fs::read_to_string(fp.trim()).unwrap()
+    } else {
+        reqwest::get(format!("{url}{fp}/fingerprint.json")).await.unwrap().text().await.unwrap()
+    };
+
     let fp_json: Value = serde_json::from_str(&contents).unwrap();
     let sha = fp_json["sha"].as_str().unwrap().trim_matches('"').to_string();
     println!("SHA: {}", sha);
@@ -84,6 +76,31 @@ async fn download(fp: String, url: String, threads: usize) {
     }
 
     for task in tasks {
-        let _ = task.await;
+        task.await.unwrap();
     }
+}
+
+fn get_config() -> (String, String, bool) {
+    let mut fp = String::new();
+    let mut url = String::new();
+
+    println!("Fingerprint File or SHA (e.g. fingerprint.json or 026b98730aac824ae292238be1176a927e139da8): ");
+    io::stdin().read_line(&mut fp).unwrap();
+
+    println!("Asset URL: (e.g. https://game-assets.brawlstarsgame.com): ");
+    io::stdin().read_line(&mut url).unwrap();
+
+    url = url.trim().to_string();
+    fp = fp.trim().to_string();
+
+    if !url.ends_with('/') {
+        url.push('/');
+    }
+
+    if fp.contains(".") {
+        return (fp, url, true);
+    } else {
+        return (fp, url, false);
+    }
+
 }
